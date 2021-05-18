0c50b757c49f534727c8a3f41bcce1a97bad8d02
Harry Lagunsad <harry.lagunsad@gmail.com>
2021-05-19 00:45:33 +0800

    fix order route with subtotal and quantity

--- controllers/orderControllers.js
+++ controllers/orderControllers.js
@@ -5,13 +5,15 @@ module.exports.createOrder = (req, res) => {
 	let orderProductId = req.body.products.map( obj => obj.productId);
 	Product.find({_id: {$in:orderProductId}})
 		.then( product => {
-			let arrProducts = req.body.products.map( obj => Object.assign(obj, {subtotal: obj.quantity*obj.Price}))
-			let arrPrice = arrProducts.map( obj => obj.subtotal)
+			let productPrice = product.map( obj => obj.Price)
+			let orderArray = productPrice.map((prices, index) => ({...req.body.products[index], Price: prices}))
+			let arrProducts = orderArray.map( obj => Object.assign(obj, {subtotal: obj.quantity*obj.Price}))			
+			let subTotal = arrProducts.map( obj => obj.subtotal)
 			let totalAmt = (total, num) => total+num;
 			let newOrder = new Order({
-				totalAmount: arrPrice.reduce(totalAmt),
+				totalAmount: subTotal.reduce(totalAmt),
 		    	userId: req.user.email,
-				products: arrProducts
+				products: arrProducts 
 			})
 			return newOrder.save()
 		})
